d073c5090a9219c6ff618d13406642aa9a777ba8
uocgh <pElOponhsou1@#>
2017-08-09 14:10:50 +0300

    Added chain calls in module, controller flow.

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,15 +1,25 @@
 /// <reference path="angular.min.js" />
 
-var myApp = angular.module ("myModule", []); //No dependencies are added.
+var myApp = angular.module("myModule", []).controller ("myController", function ($scope){
 
-myApp.controller ("myController", function ($scope){
-	
-	var employee = {
+    	var employee = {
 			Name: "Dimitris Xinidis",
 			Address: "Peloponhsou 34"
 	};
-	
+
 	$scope.message = "Test Message";
 	$scope.employee = employee;
 });
 
+// var myApp = angular.module ("myModule", []); //No dependencies are added.
+
+// myApp.controller ("myController", function ($scope){
+//
+// 	var employee = {
+// 			Name: "Dimitris Xinidis",
+// 			Address: "Peloponhsou 34"
+// 	};
+//
+// 	$scope.message = "Test Message";
+// 	$scope.employee = employee;
+// });
