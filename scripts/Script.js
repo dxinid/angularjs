3706513166e996f56aaa67cf0728e08bd7ee7aa0
uocgh <pElOponhsou1@#>
2017-08-10 12:36:22 +0300

    Use orderBy functionality of header column.
    --Arrows
    --Transition to ascending/descending.

--- scripts/Script.js
+++ scripts/Script.js
@@ -16,8 +16,22 @@ var myApp = angular.module("myModule", []).controller("myController", function (
             name: "Bill Psalto",
             address: "Panormou 10",
             gender: "Male",
-            birthdate: new Date("January 1, 1982"),
+            birthdate: new Date("January 1, 1988"),
             salary: 40600.45
+        },
+        {
+            name: "Kostas Giannaros",
+            address: "Amarousiou 10",
+            gender: "Male",
+            birthdate: new Date("March 1, 1980"),
+            salary: 50600.45
+        },
+        {
+            name: "Athina Christopoulou",
+            address: "Aristotelous 10",
+            gender: "Female",
+            birthdate: new Date("December 29, 1982"),
+            salary: 20400
         }
 
     ];
@@ -26,6 +40,37 @@ var myApp = angular.module("myModule", []).controller("myController", function (
     $scope.employees = employees;
     $scope.sortColumn = "name"; //Initial sort column when the page is loaded
 
+    $scope.ascending = true;
+
+    $scope.sortData = function (column)
+    {
+        console.log ("sortData Entering...");
+
+        if ($scope.sortColumn == column)
+        {
+            $scope.ascending = !$scope.ascending; //Reverse the direction
+        }
+
+        $scope.sortColumn = column;
+    }
+
+    $scope.getClassData = function (column)
+    {
+        console.log ("getClassData Entering... [column="+column+"]");
+        if ($scope.sortColumn == column)
+        {
+            if ($scope.ascending  == true)
+            {
+                return 'arrow-down';
+            }
+            else if ($scope.ascending  == false)
+            {
+                return 'arrow-up';
+            }
+        }
+
+        return '';
+    }
     // $scope.incrementLikes = function (technology)
     // {
     //     technology.likes++;
