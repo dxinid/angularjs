8a00d9f80bc2f879cbba9468b150c42a092f750e
uocgh <pElOponhsou1@#>
2017-08-11 17:16:53 +0300

    Custom service Part 2.
    -- Implemented the service stringTransformationService
    -- modified the CustomServiceController to integrate the service call.

--- scripts/controllers/CustomServiceController.js
+++ scripts/controllers/CustomServiceController.js
@@ -1,38 +1,11 @@
-
-var myApp = angular.module("myModule", []).controller("myController", function ($scope, $http, $log) {
-}
-);
-
-var customServiceController = myAppModule.controller ("customServiceController",
-    function ($scope)
-    {
+var customServiceController = myAppModule.controller("customServiceController",
+    function ($scope, stringTransformationService) {
 
 
-         $scope.transformString = function (inputText)
-            {
-                console.log ("[transformString] Entering...");
+        $scope.transformString = function (inputText) {
+            console.log("[transformString] Entering...");
 
-                if (!inputText)
-                {
-                    return "";
-
-                }
-
-                var output = "";
-
-                for (var i = 0 ; i < inputText.length ; i++)
-                {
-                    if (i>0 && inputText[i] == inputText[i].toUpperCase())
-                    {
-                        output = output + " ";
-                    }
-
-                    output  = output + inputText[i];
-                }
-
-                console.log ("[transformString][output="+output+"]");
-
-                $scope.output = output;
-            }
+            $scope.output = stringTransformationService.transformString(inputText);
+        }
     }
-    );
+);
